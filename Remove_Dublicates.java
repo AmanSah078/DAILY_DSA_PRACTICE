f964f0aae921267eb34465f1eb3391ff38c916a4
Aman Sah <amanalisiwan22@gmail.com>
2026-05-30 23:27:15 +0530

    Add files via upload

---
+++ Remove_Dublicates.java
@@ -0,0 +1,14 @@
+package Dsa_Once_Again.Target_50_Questions;
+
+import java.util.HashSet;
+
+public class Remove_Dublicates {
+    public static void main(String[] args) {
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        set.add(2);
+        System.out.println(set);
+    }
+}
